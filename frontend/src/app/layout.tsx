ac17cb397a3b6e2ad1077b90f49a608c10c697e1
Samuel1505 <oluwakoredeakinola@gmail.com>
2025-09-03 20:55:52 +0100

    update:fixed the broken css and added some img

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -18,8 +18,8 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
+    <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable} antialiased`}>
+      <body className="font-sans">
         <Suspense fallback={null}>{children}</Suspense>
         <Analytics />
       </body>
